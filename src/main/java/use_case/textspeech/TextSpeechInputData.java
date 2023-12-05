d6c9b698bab1ff8a2c2151f2742ccbadac181a56
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-12-04 21:59:31 -0500

    added textspeech input data

---
+++ src/main/java/use_case/textspeech/TextSpeechInputData.java
@@ -0,0 +1,17 @@
+package use_case.textspeech;
+
+
+public class TextSpeechInputData {
+    private String text;
+
+
+    public void InputData(String text) {
+        this.text = text;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+}
+
