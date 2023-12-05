28efd21e858db8cbc2c649445d04e5d79a3b96ba
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-12-05 18:40:44 -0500

    updates viewmodel and presenter to have set state, and fire property changes among others - also added TextSpeechApiClient to the repository

---
+++ src/main/java/interface_adapter/api/textspeech/TextSpeechApiClient.java
@@ -0,0 +1,44 @@
+package interface_adapter.api.textspeech;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.texttospeech.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TextSpeechApiClient {
+
+    private static final String CREDENTIALS_PATH = "api_key.json";
+
+    public void synthesizeText(String text) throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(CREDENTIALS_PATH));
+        TextToSpeechSettings textToSpeechSettings = TextToSpeechSettings.newBuilder()
+                .setCredentialsProvider(() -> credentials)
+                .build();
+
+        try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(textToSpeechSettings)) {
+            SynthesisInput input = SynthesisInput.newBuilder()
+                    .setText(text)
+                    .build();
+
+            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                    .setLanguageCode("en-US")
+                    .setSsmlGender(SsmlVoiceGender.NEUTRAL)
+                    .build();
+
+            AudioConfig audioConfig = AudioConfig.newBuilder()
+                    .build();
+
+            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+            ByteString audioContents = response.getAudioContent();
+
+            try (OutputStream out = new FileOutputStream("output.ogg")) {
+                out.write(audioContents.toByteArray());
+                System.out.println("Audio content written to file \"output.ogg\"");
+            }
+        }
+    }
+}
