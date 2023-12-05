1830c42098bfe19b4512b1198526b1b71ed6ec3d
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-12-05 17:49:31 -0500

    created TextSpeechState in Interface adapters

---
+++ src/main/java/interface_adapter/textspeech/TextSpeechState.java
@@ -0,0 +1,32 @@
+package interface_adapter.textspeech;
+
+public class TextSpeechState {
+    private String originalText = "";
+    private String spokenText = null;
+
+    public TextSpeechState(TextSpeechState copy) {
+        originalText = copy.originalText;
+        spokenText = copy.spokenText;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public TextSpeechState() {}
+
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    public String getSpokenText() {
+        return spokenText;
+    }
+
+    public void setOriginalText(String originalText) {
+        this.originalText = originalText;
+    }
+
+    public void setSpokenText(String spokenText) {
+        this.spokenText = spokenText;
+    }
+
+}
+
