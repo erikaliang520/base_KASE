82957da8ef7f71713b35be072760a84581ba0ddd
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-12-04 22:01:16 -0500

    added textspeech output data

---
+++ src/main/java/use_case/textspeech/TextSpeechOutputData.java
@@ -0,0 +1,17 @@
+package use_case.textspeech;
+
+
+public class TextSpeechOutputData {
+    private String audioContent;
+
+
+    public TextSpeechOutputData(String audioContent) {
+        this.audioContent = audioContent;
+    }
+
+
+    public String getAudioContent() {
+        return audioContent;
+    }
+}
+
