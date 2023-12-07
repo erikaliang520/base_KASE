2c2379dbe73cdd3020dbaa7bc52b637c4102e766
karkabbage <limkaryna@gmail.com>
2023-12-06 20:00:24 -0500

    Starter files for related words use case relvent tests.

---
+++ src/main/test/interface_adapter/related_words/RelatedControllerTest.java
@@ -0,0 +1,22 @@
+package interface_adapter.related_words;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelatedControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
---
+++ src/main/test/interface_adapter/related_words/RelatedViewModelTest.java
@@ -0,0 +1,24 @@
+package interface_adapter.related_words;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelatedViewModelTest { // TODO idk how to test this
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void firePropertyChanged() {
+    }
+
+    @Test
+    void addPropertyChangeListener() {
+    }
+
+    @Test
+    void getState() {
+    }
+}
