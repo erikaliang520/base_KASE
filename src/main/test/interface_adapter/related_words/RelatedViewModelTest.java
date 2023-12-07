ecf1c0f9e2fbb143e35d5ad03dc19d563731ea6f
erikaliang <erika.liang@mail.utoronto.ca>
2023-12-07 04:54:57 -0500

    finished view model tests

--- src/main/test/interface_adapter/related_words/RelatedViewModelTest.java
+++ src/main/test/interface_adapter/related_words/RelatedViewModelTest.java
@@ -1,15 +1,24 @@
 package interface_adapter.related_words;
 
+import interface_adapter.ViewModel;
+import interface_adapter.test.history.HistoryViewModelTest;
 import org.junit.jupiter.api.Test;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RelatedViewModelTest { // TODO idk how to test this
+    private RelatedState state = new RelatedState();
+    private RelatedViewModel viewModel = new RelatedViewModel();
 
     @Test
-    void setState() {
-    }
+    public void getAndSetStateTest(){
+        viewModel.setState(state);
+        assertEquals(viewModel.getState(), state);
 
+    }
     @Test
     void firePropertyChanged() {
     }
@@ -19,6 +28,48 @@ class RelatedViewModelTest { // TODO idk how to test this
     }
 
     @Test
-    void getState() {
+    public void firePropertyChangedAndAddPropertyChangeTest(){
+        InMemoryRelatedViewModel newViewModel = new InMemoryRelatedViewModel("History");
+        PropertyChangeListener listener = new MyPropertyChangeListener();
+        newViewModel.addPropertyChangeListener(listener);
+        newViewModel.firePropertyChanged();
+
+        assertEquals(newViewModel.APCL, true);
+        assertEquals(newViewModel.FPC, true);
     }
+
+    private static class InMemoryRelatedViewModel extends ViewModel {
+        private boolean APCL = false;
+        private boolean FPC = false;
+
+        public InMemoryRelatedViewModel(String viewName) {
+            super(viewName);
+        }
+
+        public void addPropertyChangeListener(PropertyChangeListener listener){
+            APCL = true;
+        }
+
+        public void firePropertyChanged(){
+            FPC = true;
+        }
+
+    }
+
+    public class MyPropertyChangeListener implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            // Handle the property change event here
+            String propertyName = evt.getPropertyName();
+            Object oldValue = evt.getOldValue();
+            Object newValue = evt.getNewValue();
+
+            System.out.println("Property changed: " + propertyName);
+            System.out.println("Old value: " + oldValue);
+            System.out.println("New value: " + newValue);
+        }
+    }
+
+
 }
